996b50a706ff53f9ce22487b1fda623161f8e709
abzal <abzal008@mail.ru>
2022-10-22 20:33:06 +0600

    first commit

--- src/cnb-1-main/config.ts
+++ src/cnb-1-main/config.ts
@@ -1,6 +1,6 @@
-const USER_NAME = process.env.MONGO_DB_USER_NAME || "lesson5";
-const PASSWORD = process.env.MONGO_DB_USER_PASSWORD || "1qaz2wsx";
-const MONGO_DB_URL = process.env.MONGO_DB_URL || "cluster0.gbcdgvc.mongodb.net/nekobd"; // bd for tests
+const USER_NAME = process.env.MONGO_DB_USER_NAME || "suan";
+const PASSWORD = process.env.MONGO_DB_USER_PASSWORD || "7777";
+const MONGO_DB_URL = process.env.MONGO_DB_URL || "cluster0.xyyt9t9.mongodb.net/"; // bd for tests
 
 export const MongoDBUris = `mongodb+srv://${USER_NAME}:${PASSWORD}@${MONGO_DB_URL}?retryWrites=true&w=majority`;
 console.log({MongoDBUris})
